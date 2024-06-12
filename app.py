6b75e6b5eaf74dfff28f0f946b7edf87b0dfba3c
chinpeerapat <88846925+Chinpeerapat@users.noreply.github.com>
2024-06-13 01:35:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os
 import streamlit as st
-import anthropic
 from dotenv import load_dotenv
+from anthropic import Anthropic
 
-
-
-anthropic_api_key = os.environ.get("ANTHROPIC_API_KEY")
-
+client = Anthropic(
+    # This is the default and can be omitted
+    api_key=os.environ.get("ANTHROPIC_API_KEY"),
+)
     
 # with st.sidebar:
 #     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
@@ -21,19 +21,18 @@ question = st.text_input(
     disabled=not uploaded_file,
 )
 
-if uploaded_file and question and not anthropic_api_key:
+if uploaded_file and question and not api_key:
     st.info("Please add your Anthropic API key to continue.")
 
-if uploaded_file and question and anthropic_api_key:
+if uploaded_file and question and api_key:
     article = uploaded_file.read().decode()
     prompt = f"""{anthropic.HUMAN_PROMPT} Here's an article:\n\n
     {article}\n\n\n\n{question}{anthropic.AI_PROMPT}"""
 
-    client = anthropic.Client(api_key=anthropic_api_key)
     response = client.completions.create(
         prompt=prompt,
         stop_sequences=[anthropic.HUMAN_PROMPT],
-        model="claude-v1", #"claude-2" for Claude 2 model
+        model="claude-3-opus-20240229",
         max_tokens_to_sample=100,
     )
     st.write("### Answer")
